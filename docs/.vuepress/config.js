74145cb98b785ddfe51371298b12efc0b7a0e776
Kaj Lund <kaj.lund@gmail.com>
2018-12-29 23:54:26 +0200

    Added some NoSQL doc and meta

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'rannikkohurri.net',
+    description: 'LuKa\'s Personal Blog',
+    head: [
+        [ 'link', { rel: 'icon', href: '/favicon.ico'} ]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        lastUpdated: 'Updated'
+        navbar: true,
+        nav: [
+            { text: 'NoSQL', link: '/NoSQL/' }
+        ],
+        sidebar: {
+            '/NoSQL/': [
+                '',
+                'MongoDB',
+                'Redis'
+            ]
+        }
+    }
+}
---
+++ docs/NoSQL/MongoDB.md
@@ -0,0 +1,9 @@
+---
+title: MongoDB
+lang: en-US
+description: The ins- and outs of using MongoDB
+---
+
+### Badge <Badge text="beta" type="warn"/> <Badge text="0.1.0+"/>
+
+More to come here...
